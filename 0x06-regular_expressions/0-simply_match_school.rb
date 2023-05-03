4f8b51435825299f0029320910e50a95e0dad0ff
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-05-03 13:02:03 +0000

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+pattern = /School/
+
+if ARGV.length != 1
+  puts "Usage: ./0-simply_match_school.rb <string>"
+  exit(1)
+end
+
+string = ARGV[0]
+if pattern.match(string)
+  puts "Match found: #{pattern.match(string)}"
+else
+  puts "No match found."
+end
