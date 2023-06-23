a2c6925434247f48f592cb9f7e9cacb25adb068b
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-06-23 21:22:12 +0000

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installs puppet-lint, version 2.1.1
+# Installs flask, version 2.1.0
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
+package { 'flask':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
